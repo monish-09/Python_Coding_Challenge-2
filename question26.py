f037b44a2066475fde1054e6c1dcbcff74aa35c6
monish-09 <mkha22647@gmail.com>
2026-04-09 10:45:34 +0530

    Add files via upload

---
+++ question26.py
@@ -0,0 +1,10 @@
+text = input("Enter sentence: ")
+
+words = text.split()
+longest = words[0]
+
+for w in words:
+    if len(w) > len(longest):
+        longest = w
+
+print("Longest word:", longest)
