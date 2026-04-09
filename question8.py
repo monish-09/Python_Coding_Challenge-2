84dc471206f44573da8b596f5a5e0ce5a42c186b
monish-09 <mkha22647@gmail.com>
2026-04-09 09:59:07 +0530

    Add files via upload

---
+++ question8.py
@@ -0,0 +1,9 @@
+number = int(input("enter the marks: "))
+if (number>90 and number<=100):
+    print("Grade A")
+elif (number>80 and number<90):
+    print("Grade B")
+elif (number>60 and number<80):
+    print("Grade C")
+else:
+    print("Grade f")
